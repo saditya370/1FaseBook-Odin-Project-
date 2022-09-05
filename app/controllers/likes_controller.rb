7430787d317c01ae2cbf577bd37f4dd6bf43ded9
Aditya Singh <saditya370@gmail.com>
2022-09-05 12:56:45 +0530

    Added Like fuctionality to fb clone

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,13 @@ class LikesController < ApplicationController
     
     def create  
         
-            @like = current_user.likes.new(like_params)
+            @like = current_user.likes.build(like_params)
             if !@like.save
                 flash[:notice] = @like.errros.full_messages.to_sentences 
             end
 
-            redirect_to @like.post
+            # redirect_to @like.post
+            redirect_to request.referrer
 
     end
 
@@ -17,7 +18,7 @@ class LikesController < ApplicationController
         @like = current_user.likes.find(params[:id])
         post = @like.post  
         @like.destroy  
-        redirect_to post
+        redirect_to request.referrer
 
     end
 
