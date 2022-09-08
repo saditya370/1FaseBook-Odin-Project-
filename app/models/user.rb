aa12be9fdf026c256b1f00c3b4457ed3080145f0
Aditya Singh <saditya370@gmail.com>
2022-09-08 18:52:49 +0530

    removed some bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-
+  validates :image ,presence: true
 
          has_many :posts,dependent: :destroy
          has_many :likes, dependent: :destroy
